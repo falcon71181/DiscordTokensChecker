12e8670217f5e4cbaee5e6fa014cac8c12626de0
Abhay Thakur <48170773+falcon71181@users.noreply.github.com>
2023-09-16 12:10:52 +0530

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,1 +1,159 @@
+from urllib.request import Request, urlopen
+from json import loads, dumps
 
+def GetTokenInfo(token):
+    headers = {
+        "Authorization": token,
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0"
+    }
+
+    userjson = loads(urlopen(Request("https://discordapp.com/api/v6/users/@me", headers=headers)).read().decode())
+    username = userjson["username"]
+    hashtag = userjson["discriminator"]
+    email = userjson["email"]
+    idd = userjson["id"]
+    pfp = userjson["avatar"]
+    flags = userjson["public_flags"]
+    nitro = ""
+    phone = "-"
+
+    if "premium_type" in userjson:
+        nitrot = userjson["premium_type"]
+        if nitrot == 1:
+            nitro = "<:classic:896119171019067423> "
+        elif nitrot == 2:
+            nitro = "<a:boost:824036778570416129> <:classic:896119171019067423> "
+    if "phone" in userjson: phone = f'`{userjson["phone"]}`'
+
+    return username, hashtag, email, idd, pfp, flags, nitro, phone
+
+def GetUHQFriends(token):
+    badgeList = [
+        {"Name": 'Early_Verified_Bot_Developer', 'Value': 131072, 'Emoji': "<:developer:874750808472825986> "},
+        {"Name": 'Bug_Hunter_Level_2', 'Value': 16384, 'Emoji': "<:bughunter_2:874750808430874664> "},
+        {"Name": 'Early_Supporter', 'Value': 512, 'Emoji': "<:early_supporter:874750808414113823> "},
+        {"Name": 'House_Balance', 'Value': 256, 'Emoji': "<:balance:874750808267292683> "},
+        {"Name": 'House_Brilliance', 'Value': 128, 'Emoji': "<:brilliance:874750808338608199> "},
+        {"Name": 'House_Bravery', 'Value': 64, 'Emoji': "<:bravery:874750808388952075> "},
+        {"Name": 'Bug_Hunter_Level_1', 'Value': 8, 'Emoji': "<:bughunter_1:874750808426692658> "},
+        {"Name": 'HypeSquad_Events', 'Value': 4, 'Emoji': "<:hypesquad_events:874750808594477056> "},
+        {"Name": 'Partnered_Server_Owner', 'Value': 2, 'Emoji': "<:partner:874750808678354964> "},
+        {"Name": 'Discord_Employee', 'Value': 1, 'Emoji': "<:staff:874750808728666152> "}
+    ]
+    headers = {
+        "Authorization": token,
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0"
+    }
+    try:
+        friendlist = loads(
+            urlopen(Request("https://discord.com/api/v6/users/@me/relationships", headers=headers)).read().decode())
+    except:
+        return False
+
+    uhqlist = ''
+    for friend in friendlist:
+        OwnedBadges = ''
+        flags = friend['user']['public_flags']
+        for badge in badgeList:
+            if flags // badge["Value"] != 0 and friend['type'] == 1:
+                if not "House" in badge["Name"]:
+                    OwnedBadges += badge["Emoji"]
+                flags = flags % badge["Value"]
+        if OwnedBadges != '':
+            uhqlist += f"{OwnedBadges} | {friend['user']['username']}#{friend['user']['discriminator']} ({friend['user']['id']})\n"
+    return uhqlist
+
+
+def GetBilling(token):
+    headers = {
+        "Authorization": token,
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0"
+    }
+    try:
+        billingjson = loads(urlopen(
+            Request("https://discord.com/api/users/@me/billing/payment-sources", headers=headers)).read().decode())
+    except:
+        return False
+
+    if billingjson == []: return " -"
+
+    billing = ""
+    for methode in billingjson:
+        if methode["invalid"] == False:
+            if methode["type"] == 1:
+                billing += ":credit_card:"
+            elif methode["type"] == 2:
+                billing += ":parking: "
+
+    return billing
+
+
+def GetBadge(flags):
+    if flags == 0: return ''
+
+    OwnedBadges = ''
+    badgeList = [
+        {"Name": 'Early_Verified_Bot_Developer', 'Value': 131072, 'Emoji': "<:developer:874750808472825986> "},
+        {"Name": 'Bug_Hunter_Level_2', 'Value': 16384, 'Emoji': "<:bughunter_2:874750808430874664> "},
+        {"Name": 'Early_Supporter', 'Value': 512, 'Emoji': "<:early_supporter:874750808414113823> "},
+        {"Name": 'House_Balance', 'Value': 256, 'Emoji': "<:balance:874750808267292683> "},
+        {"Name": 'House_Brilliance', 'Value': 128, 'Emoji': "<:brilliance:874750808338608199> "},
+        {"Name": 'House_Bravery', 'Value': 64, 'Emoji': "<:bravery:874750808388952075> "},
+        {"Name": 'Bug_Hunter_Level_1', 'Value': 8, 'Emoji': "<:bughunter_1:874750808426692658> "},
+        {"Name": 'HypeSquad_Events', 'Value': 4, 'Emoji': "<:hypesquad_events:874750808594477056> "},
+        {"Name": 'Partnered_Server_Owner', 'Value': 2, 'Emoji': "<:partner:874750808678354964> "},
+        {"Name": 'Discord_Employee', 'Value': 1, 'Emoji': "<:staff:874750808728666152> "}
+    ]
+    for badge in badgeList:
+        if flags // badge["Value"] != 0:
+            OwnedBadges += badge["Emoji"]
+            flags = flags % badge["Value"]
+
+    return OwnedBadges
+
+def checkToken(token):
+    headers = {
+        "Authorization": token,
+        "Content-Type": "application/json",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0"
+    }
+    try:
+        urlopen(Request("https://discordapp.com/api/v6/users/@me", headers=headers))
+        return True
+    except:
+        return False
+
+def checkker(file_name):
+    with open(file_name,'r') as file:
+        tokens = file.readlines()
+        for token in tokens:
+            if checkToken(token):
+                print_info(token)
+            else:
+                pass
+
+def print_info(token):
+    username, hashtag, email, idd, pfp, flags, nitro, phone = GetTokenInfo(token)
+    discord_name = username + hashtag
+    print(f"""
+##################################################
+##    Token    : {token}
+##		UserName : {discord_name}
+##		 Email	 : {email}		
+##		 Nitro	 : {nitro}		
+##		 Phone	 : {phone}		
+##		User ID	 : {idd}		
+## ProfilePic ID : {pfp}		
+## 	Public Flags : {flags}		
+## 	Billing		 : {GetBilling(token)}		
+## 	UHQFriends	 : {GetUHQFriends(token)}		
+## 		Badge	 : {GetBadge(flags)}		
+##################################################""")
+
+
+if __name__ == '__main__':
+    file_name = input("Enter Token filename / path : ")
+    checkker(file_name)
